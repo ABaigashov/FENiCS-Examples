13ba55b0f71a69a25ca0c6d37a146af969d0675b
AstroBFU <astrobfu@gmail.com>
2019-05-07 15:32:22 +0200

    test

--- Neumann_boundary.py
+++ Neumann_boundary.py
@@ -4,7 +4,7 @@ from fenics import *
 # Create mesh and define function space
 mesh = UnitSquareMesh(8, 8)
 V = FunctionSpace(mesh, 'P', 1)
-   
+
 # Define Dirichlet boundary condition
 u_D = Expression('1 + x[0]*x[0] + x[1]*x[1]', degree = 2)
 
@@ -18,7 +18,7 @@ def boundary_D(x, on_boundary):
             return False
     else:
         return False
-
+ 
 # Define Neuman boundary condition
 g = Expression('0', degree = 1)
 
