279babe9f17c7ec5b92c2033ab19430b747194ef
AstroBFU <astrobfu@gmail.com>
2019-05-10 13:48:26 +0200

    Update Horror.py

--- Horror.py
+++ Horror.py
@@ -18,12 +18,12 @@ a=0.001
 
 
 
-domain =fen.Circle(Point(0, 0), R)
+domain =Circle(Point(0, 0), R)
 
-cylinder = fen.Circle(Point(0, 0), b) - fen.Circle(Point(0, 0), a)
+cylinder = Circle(Point(0, 0), b) - Circle(Point(0, 0), a)
 
 domain.set_subdomain(1, cylinder)
 
-mesh = fen.generate_mesh(domain, 32)
+mesh = generate_mesh(domain, 32)
 
 plot(mesh)
