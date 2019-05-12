630882d04995dd5ca28f9e97e22a9469c5847f1b
ABaigashov <a.baigashov@gmail.com>
2019-05-12 22:51:11 +0200

    Доработал скрипт Star3d
    Старый вывод закомментировал

--- Star3d.py
+++ Star3d.py
@@ -15,6 +15,31 @@ mesh=generate_mesh(domain, 32)
 
 plot(mesh)
 
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition
+u_D = Expression('1 + x[0]*x[0] + x[1]*x[1] + x[2]', degree=2)
+
+def boundary(x, on_boundary):
+	return on_boundary
+
+bc = DirichletBC(V, u_D, boundary)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Constant(-6.0)
+a = dot(grad(u), grad(v))*dx
+L = f*v*dx
+
+# Compute solution
+u = Function(V)
+solve(a == L, u, bc)
+
+# Save to file and plot
+File("classic.pvd") << u
+
+'''
 V = FunctionSpace(mesh, 'P', 2)
 
 bc = DirichletBC(V, Constant(0), 'on_boundary')
@@ -37,3 +62,4 @@ solve(a == L, nu, bc)
 plot(nu)
 
 #plot(mesh)
+'''
