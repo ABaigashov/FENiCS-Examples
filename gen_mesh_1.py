a4b6ae274504a8732ca533342ebfc68d1927e4d5
AstroBFU <astrobfu@gmail.com>
2019-05-15 12:46:15 +0200

    Sum

--- gen_mesh_1.py
+++ gen_mesh_1.py
@@ -5,11 +5,11 @@ R=10
 
 domain =  mshr.Rectangle(fenics.Point(0., 0.), fenics.Point(5., 5.)) \
          - mshr.Rectangle(fenics.Point(2., 1.25), fenics.Point(3., 1.75)) \
-         - mshr.Circle(fenics.Point(1, 4), .25) \
-         + mshr.Circle(fenics.Point(7, 7), 1.25)
+         - mshr.Circle(fenics.Point(1, 4), .25) #\
+         #+ mshr.Circle(fenics.Point(7, 7), 1.25)
 
-mesh = mshr.generate_mesh(domain, 132)
-
+mesh = mshr.generate_mesh(domain, 164)
+#
 V = fenics.FunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
@@ -20,7 +20,7 @@ def boundary(x, on_boundary):
 
 bc = fenics.DirichletBC(V, u_D, boundary)
 
-# Define variational problem
+# # Define variational problem
 u = fenics.TrialFunction(V)
 v = fenics.TestFunction(V)
 f = fenics.Constant(-6.0)
@@ -31,10 +31,8 @@ L = f*v*fenics.dx
 u = fenics.Function(V)
 s = fenics.solve(a == L, u, bc)
 
-print(type(s))
-print(type(u))
 
-'''
+
 #--------------------------Ploting pylab------------------------
 #get array componets and triangulation :
 v = u.compute_vertex_values(mesh)
@@ -50,10 +48,9 @@ cm = plb.get_cmap('viridis')
 ax.tricontourf(x, y, t, v, 10, cmap = cm)
 ax.triplot(x, y, t, '-', color='k', lw=0.2, alpha=0.4)
 
-plb.ylim(0, 10)
-plb.xlim(0, 10)
+plb.ylim(0, 5)
+plb.xlim(0, 5)
 
 # Output in the file
 print("Lenear_Poisson.png")
 plb.savefig("results/gen_mesh.%s" % "png", bbox_inches="tight")
-'''
