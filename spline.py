e9eed455a950a796f81353f7c0bebed741041d1e
ABaigashov <a.baigashov@gmail.com>
2019-05-17 16:07:23 +0200

    Create test.py; update spline.py

--- spline.py
+++ spline.py
@@ -1,14 +1,23 @@
-from scipy.interpolate import splev, splrep
+from scipy.interpolate import splev, splrep, InterpolatedUnivariateSpline, interp1d
 import sympy as sym
+import matplotlib.pyplot as plt
+import numpy as np
 
-def f(x):
-    x_points = [ 0, 1, 2, 3, 4, 5]
-    u_points = [12,14,22,39,58,77]
 
-    tmp = splrep(x_points, y_points)
+x_points = [0, 0.3, 0.4, 0.1, 0.6, 0.7]
+y_points = [1., 2., 5., 10., 99., 88]
+x = np.linspace(0, 10, num=11, endpoint=True)
+y = np.cos(-x**2/9.0)
+f = interp1d(x, y)
+f2 = interp1d(x, y, kind='cubic')
 
-    return splev(x, tmp)
+xnew = np.linspace(0, 10, num=41, endpoint=True)
 
+print(f(xnew))
+
+plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
+plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.show()
 
 
 # x = sym.symbols('x[0]')
