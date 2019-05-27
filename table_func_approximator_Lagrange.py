46d4aba6822c580869a2943d3c959408e7f45e12
ABaigashov <a.baigashov@gmail.com>
2019-05-27 13:08:33 +0200

    Update

--- table_func_approximator_Lagrange.py
+++ table_func_approximator_Lagrange.py
@@ -1,6 +1,23 @@
 from scipy.interpolate import lagrange
 import numpy as np
+import sympy as sym
 
-x = np.array([0, 1, 2])
-y = x**3
-poly = lagrange(x, y)
+f = open('table_func.txt', 'r')
+lines = f.read().split()
+f.close()
+
+x = sym.symbols('x[0]')
+
+abscissa = []
+ordinate = []
+for i in range(0, len(lines)-1, 2):
+    abscissa.append(float(lines[i]))
+    ordinate.append(float(lines[i+1]))
+
+poly = lagrange(abscissa, ordinate)
+
+
+
+f_code = sym.printing.ccode(poly)
+
+print(f_code)
