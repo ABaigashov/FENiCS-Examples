e8a28e990261fc1c150b057ee235ecc2775b1baa
ABaigashov <a.baigashov@gmail.com>
2019-05-27 12:11:06 +0200

    Summary updates

--- table_func_approximator.py
+++ table_func_approximator.py
@@ -2,9 +2,12 @@ from sympy import *
 
 def approximator(f):
     """ Функция возвращает, аппроксимированную десятичными логарифмами,
-        таблично-заданную функцию, в терминах C++
+        таблично-заданную функцию y = f(x), в терминах C++.
+
+        На вход функции подается открытый для чтения файл формата .txt
+        (f = open('table_func.txt', 'r'))
     """
-    # Чтение списка
+    # Чтение и разбиение строк списка
     lines = f.read().split()
     f.close()
 
@@ -15,22 +18,22 @@ def approximator(f):
         abscissa.append(float(lines[i]))
         ordinate.append(float(lines[i+1]))
 
-    # Количество строк в списке
+    # Определение количества строк в списке
     N = len(abscissa)
 
-    # Создание списка с параметрами для уравнения a*log(x + c)
-    k_a = []
+    # Создание списка с неизвестными a_i параметрами для уравнения a*log(x + c)
+    a_i = []
     for i in range(0, N, 1):
-        k_a.append('a' + str(i))
-        k_a[i] = symbols(k_a[i])
+        a_i.append('a' + str(i))
+        a_i[i] = symbols(a_i[i])
 
     # Создание системы аппроксимирующих функций с неизвестными параметрами
+    # Где c = N - i
     x = symbols('x')
     f = 0
-
     for i in range(0, N, 1):
-        k = k_a[i]*log(x + N - i)
-        f = f + k
+        a = a_i[i]*log(x + N - i)
+        f = f + a
 
     # Определение системы уравнений с неизвестными параметрами
     eq = []
@@ -42,21 +45,22 @@ def approximator(f):
     for i in range(0, N, 1):
         x0.append(i)
 
-    # Определение неизвестных коэффициентов
-    sol = solvers.nsolve((eq), (k_a), (x0))
+    # Определение неизвестных параметров
+    sol = solvers.nsolve((eq), (a_i), (x0))
 
-    # Построение итоговой функции с определенными коэффициентами
+    # Построение итоговой функции с определенными параметрами
     x = symbols('x[0]')
     func = 0
     for i in range(0, N, 1):
-        k = sol[i]*log(x + N - i)
-        func = func + k
+        a = sol[i]*log(x + N - i)
+        func = func + a
 
     # Принтинг функции в язык С++
     f_code = printing.ccode(func)
 
-    print(f_code)
+    #print(f_code)
 
     return f_code
 
-#approximator()
+# f = open('table_func.txt', 'r')
+# print(approximator(f))
