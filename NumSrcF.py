f92ea61420b0de7191b5088e770242dbaa0d628a
AstroBFU <astrobfu@gmail.com>
2019-06-03 00:34:29 +0200

    Sum
    Навел порядок в репозитории, оформил файл NumSrc, для расчета функции источника по уравнению состояния и функцией энтальпии в виде таблицы

--- NumSrcF.py
+++ NumSrcF.py
@@ -1,13 +1,16 @@
 from fenics import *
 import numpy as np
 from mshr import *
+import sympy as smp
+from enthalpy0 import generate_src_function
+import matplotlib.pyplot as plt
 
 # solve -laplace(u) = f in [0, 1]^2 with homog. Dirichlet boundary conditions
 # and funny 'numerical' rhs
 
 #mesh = RectangleMesh(-1,-1,1,1,10,10,'right')
 
-domain=Rectangle(Point(-1,-1), Point(10, 10))
+domain=Rectangle(Point(0,0), Point(10, 10))
 
 mesh=generate_mesh(domain, 32)
 
@@ -18,37 +21,106 @@ n = F.dim()
 
 print(n)
 
-
-
 d = mesh.geometry().dim()
 
 print(d)
 
-
 f = Function(F)
 
-print(f)
-
-
-
+f1 = Function(F)
 
 # dofmap approach
 F_dof_coordinates = F.tabulate_dof_coordinates().reshape(n,d)
-
-print(F_dof_coordinates[10])
 
 F_dof_coordinates.resize((n, d))
 F_dof_x = F_dof_coordinates[:, 0]
 F_dof_y = F_dof_coordinates[:, 1]
 
 f_values = np.zeros(n)
-f_values = abs(F_dof_x + F_dof_y)
+
+g_values=np.zeros(n)
+
+Name = 'data/EOS.txt'
+x=smp.Symbol('x')
+y=smp.Symbol('y')
+g0=0.45
+
+R0=5
+
+g=g0*(1-x*x/(R0*R0))
+
+N=10
+
+X0=1
+Y0=0
+
+G = open(Name, 'r')
+
+lines = G.read().split()
+
+G.close()
+
+
+G = open('data/H.txt', 'w')
+for j in range(n):
+    X=F_dof_x[j]
+    Y=F_dof_y[j]
+    enthalpy0=g.subs(x,X)
+    enthalpy0=enthalpy0.subs(y,Y)
+    a=[]
+    a.append(X)
+    a.append(Y)
+    a.append(enthalpy0)
+    G.write(str(a[0]))
+    G.write(' ')
+    G.write(str(a[1]))
+    G.write(' ')
+    G.write(str(a[2]))
+    G.write(' ')
+    G.write('\n')
+
+G.close()
+
+F1=open('data/H.txt','r')
+
+lines1 = F1.read().split()
+
+F1.close()
+
+x = []
+y= []
+enth = []
+
+for i in range(0, len(lines1)-2, 3):
+    enth.append(float(lines1[i+2]))
+    x.append(float(lines1[i]))
+    y.append(float(lines1[i+1]))
+
+for i in range(n):
+    enthalpy0=enth[i]
+    result=generate_src_function(lines,enthalpy0,F_dof_x[i],F_dof_y[i])
+    f_values[i]=result[2]
+    g_values[i]=result[3]
+
+
+#f_values = abs(F_dof_x + F_dof_y)
 f.vector()[:] = f_values
+f1.vector()[:] = g_values
 plot(f, title='Source term')
 
 
-print(F_dof_coordinates[1000])
-print(f_values[1000])
+print(F_dof_coordinates[n-1])
+print(f_values[n-1])
+print(g_values[n-1])
+
+
+fig=plt.figure(figsize=(8,6), facecolor='pink', frameon=True)
+plt.scatter(F_dof_x,f_values)
+plt.scatter(F_dof_x,g_values)
+plt.savefig('results/Src.png')
+plt.show()
+
+
 
 # # vertex to dofmap approach
 # vertex_values = np.zeros(mesh.num_vertices())
@@ -61,6 +133,8 @@ print(f_values[1000])
 # plot(f, title='Source term')
 
 # variational problem definition
+
+'''
 V = FunctionSpace(mesh, 'CG', 2)
 
 u = TrialFunction(V)
@@ -74,4 +148,5 @@ u = Function(V)
 solve(a == L, u, bc)
 
 plot(u, title='Solution')
-interactive()
+'''
+#interactive()
--- enthalpy0.py
+++ enthalpy0.py
@@ -1,81 +1,82 @@
 import numpy as np
-from sympy import *
+import sympy as smp
 import math
 
-f0=0.45
 
-R0=5
+def generate_src_function(lines,enthalpy0,X0,Y0):
 
-x=Symbol('x')
+    density = []
+    pressure = []
+    enthalpy = []
 
-N=20
+    for i in range(0, len(lines)-2, 3):
+        enthalpy.append(float(lines[i]))
+        density.append(float(lines[i+1]))
+        pressure.append(float(lines[i+2]))
 
-f=f0*(1-x*x/(R0*R0))
+    def energy(X0,Y0,enthalpy0):
+        for i in range(len(pressure)):
+            if enthalpy0<0:
+                result1=0
+                result2=0
+                break
+            else:
+                for j in range(len(pressure)):
+                    if enthalpy0<enthalpy[j]:
+                        K1=(pressure[j]-pressure[j-1])/(math.exp(enthalpy[j])-math.exp(enthalpy[j-1]))
+                        K2=(density[j]-density[j-1])/(math.exp(enthalpy[j])-math.exp(enthalpy[j-1]))
+                        result1=pressure[j-1]+(math.exp(enthalpy0)-math.exp(enthalpy[j-1]))*K1
+                        result2=density[j-1]+(math.exp(enthalpy0)-math.exp(enthalpy[j-1]))*K2
+                        break
+        return X0,Y0,result2,result1
+    result=energy(X0,Y0,enthalpy0)
+    return result
 
 
-density = []
-pressure = []
-enthalpy = []
 
+'''
 F = open('data/EOS.txt', 'r')
 
 lines = F.read().split()
 
 F.close()
 
-for i in range(0, len(lines)-2, 3):
-    enthalpy.append(float(lines[i]))
-    density.append(float(lines[i+1]))
-    pressure.append(float(lines[i+2]))
-
-x0=6
-
-
-def energy(X0,enthalpy0):
-    for i in range(len(pressure)):
-        if enthalpy0<0:
-            result1=0
-            result2=0
-            break
-        else:
-            for j in range(len(pressure)):
-                if enthalpy0<enthalpy[j]:
-                    K1=(pressure[j]-pressure[j-1])/(math.exp(enthalpy[j])-math.exp(enthalpy[j-1]))
-                    K2=(density[j]-density[j-1])/(math.exp(enthalpy[j])-math.exp(enthalpy[j-1]))
-                    result1=pressure[j-1]+(math.exp(enthalpy0)-math.exp(enthalpy[j-1]))*K1
-                    result2=density[j-1]+(math.exp(enthalpy0)-math.exp(enthalpy[j-1]))*K2
-                    break
-    return X0,result2,result1
-
-radii = []
+x = []
+y= []
 enth = []
 
-F1=open('H.txt','r')
+F1=open('data/H1.txt','r')
 
-lines = F1.read().split()
+lines1 = F1.read().split()
 
 F1.close()
 
-for i in range(0, len(lines)-1, 2):
-    enth.append(float(lines[i+1]))
-    radii.append(float(lines[i]))
+for i in range(0, len(lines1)-2, 3):
+    enth.append(float(lines1[i+2]))
+    x.append(float(lines1[i]))
+    y.append(float(lines1[i+1]))
 
 
 
-G = open('Table.txt', 'w')
-for i in range(len(radii)):
-    X=radii[i]
-    enthalpy0=enth[i]-0.01
-    RES=energy(X,enthalpy0)
+G = open('data/Table1.txt', 'w')
+for i in range(len(x)):
+    X=x[i]
+    Y=y[i]
+    enthalpy0=enth[i]
+    RES=generate_src_function(lines,enthalpy0,X,Y)
     a=[]
     a.append(RES[0])
     a.append(RES[1])
     a.append(RES[2])
+    a.append(RES[3])
     G.write(str(a[0]))
     G.write(' ')
     G.write(str(a[1]))
     G.write(' ')
     G.write(str(a[2]))
+    G.write(' ')
+    G.write(str(a[3]))
     G.write('\n')
 
 G.close()
+'''
