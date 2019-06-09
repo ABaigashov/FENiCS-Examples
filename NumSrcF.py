26de88575554fcea464a1e797a0eaad5cc426aa3
AstroBFU <astrobfu@gmail.com>
2019-06-09 20:13:34 +0200

    Comments
    Даны подробные комментарии к файлам System-2.py, NumSrc.py и enthalpy0.py. Эти файлы используются для решения задачи о невращающейся нейтронной звезде. System-2 - основной файл, два других содержат функции, требующиеся для расчетов.

--- NumSrcF.py
+++ NumSrcF.py
@@ -6,24 +6,40 @@ from enthalpy0 import generate_src_function
 import matplotlib.pyplot as plt
 
 
-# solve -laplace(u) = f in [0, 1]^2 with homog. Dirichlet boundary conditions
-# and funny 'numerical' rhs
+"""
+Функция для расчета плотности и давления в узлах координатной сетки.
 
-#mesh = RectangleMesh(-1,-1,1,1,10,10,'right')
+Входные параметры:
+
+Name1 - имя файла, содержащего значения энтальпии в узлах сетки.
+Формат: x-координата узла - у-координата узла - энтальпия
+
+Name2 - имя файла, содержащего информацию об уравнении состояния.
+Формат: энтальпия - плотность - давление. Значения должны быть в порядке возрастания
+в столбцах
+
+n - количество узлов сетки, т.е. фактически количество строк в файле Name1.
+Можно переписать код так, что этот аргумент не будет требоааться.
+
+Выходные данные: два списка -  ллотность и давление в узлах сетки
+
+"""
 
 def numer_src_function(Name1,Name2,n):
     # source term calculation
 
+    #объявление списков
     f_values = np.zeros(n)
-
     g_values=np.zeros(n)
 
+    #открытие файла со значеииями энтальпии в узлах сетки
     G = open(Name1, 'r')
 
     lines = G.read().split()
 
     G.close()
 
+    #открытие файла с рравнением состояния и считывание
     F1=open(Name2,'r')
 
     lines1 = F1.read().split()
@@ -33,12 +49,13 @@ def numer_src_function(Name1,Name2,n):
     x = []
     y= []
     enth = []
-
+    #считывание х- и y-координат узлов и значения энтальпии
     for i in range(0, len(lines1)-2, 3):
         enth.append(float(lines1[i+2]))
         x.append(float(lines1[i]))
         y.append(float(lines1[i+1]))
 
+    #вызов функции, которая по энтальпии дает значение плотности и давления 
     for i in range(n):
         enthalpy0=enth[i]
         result=generate_src_function(lines,enthalpy0,x[i],y[i])
