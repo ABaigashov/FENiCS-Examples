0f7d96eacd910837d999c84c295b318a5baada2c
AstroBFU <astrobfu@gmail.com>
2019-06-10 12:25:20 +0200

    Sum update

--- System-2.py
+++ System-2.py
@@ -52,7 +52,7 @@ F_dof_y = F_dof_coordinates[:, 1]
 g0=0.45
 
 #приблизительный радиус звезды (для первого шага)
-R0=5
+R0=5.2
 
 x=sym.Symbol('x')
 y=sym.Symbol('y')
@@ -207,7 +207,7 @@ F=-f1*u_1.dx(0)*v_1.dx(0)*dx-f1*f0*f0*u_1.dx(1)*v_1.dx(1)*dx \
 -f2*u_2.dx(0)*v_2.dx(0)*dx-f2*f0*f0*u_2.dx(1)*v_2.dx(1)*dx \
 +f1*v_1*u_1.dx(0)*u_2.dx(0)*dx+f1*f0*f0*v_1*u_1.dx(1)*u_2.dx(1)*dx \
 +f2*v_2*u_2.dx(0)*u_2.dx(0)*dx+f2*f0*f0*v_2*u_2.dx(1)*u_2.dx(1)*dx \
--f1*4*(np.pi)*(f+3*f00)*exp(2*u_2-2*u_1)*v_1*dx \
+-f1*4*(np.pi)*(0*f+3*f00)*exp(2*u_2-2*u_1)*v_1*dx \
 -f2*exp(2*u_2-2*u_1)*4*4*(np.pi)*f00*v_2*dx
 
 
@@ -321,7 +321,7 @@ for k in range(8):
     -f2*u_2.dx(0)*v_2.dx(0)*dx-f2*f0*f0*u_2.dx(1)*v_2.dx(1)*dx \
     +f1*v_1*u_1.dx(0)*u_2.dx(0)*dx+f1*f0*f0*v_1*u_1.dx(1)*u_2.dx(1)*dx \
     +f2*v_2*u_2.dx(0)*u_2.dx(0)*dx+f2*f0*f0*v_2*u_2.dx(1)*u_2.dx(1)*dx \
-    -f1*4*(np.pi)*(f+3*f00)*exp(2*u_2-2*u_1)*v_1*dx \
+    -f1*4*(np.pi)*(0*f+3*f00)*exp(2*u_2-2*u_1)*v_1*dx \
     -f2*exp(2*u_2-2*u_1)*4*4*(np.pi)*f00*v_2*dx
 
     solve(F == 0, u, bcs)
@@ -352,4 +352,94 @@ for k in range(8):
     fig=plt.figure(figsize=(8,6), facecolor='pink', frameon=True)
     plt.plot(data_x,data_y2)
     plt.savefig('results/Profile2-1.png')
+    plt.show()
+
+
+for k in range(8):
+    #вычисление nu0 - значения nu=u_1 в центре
+    p0=Point(0.0001,0)
+    nu0=u(p0.x(), p0.y())[0]
+    #вывод на экран необязателен, просто для контроля
+    print(nu0)
+
+    #запись пересчитннной энтальпии в узлах сетки в файл H1.txt.
+    G = open('data/H1.txt', 'w')
+
+    for i in range(n):
+        a=[]
+        p=Point(F_dof_x[i], F_dof_y[i])
+        nu=u(p.x(), p.y())[0]
+        #пересчет энтальпии H в данном узле
+        H=g0+nu0-nu
+        X=F_dof_x[i]
+        Y=F_dof_y[i]
+        a.append(H)
+        G.write(str(X))
+        G.write(' ')
+        G.write(str(Y))
+        G.write(' ')
+        G.write(str(a[0]))
+        G.write('\n')
+
+    G.close()
+
+    """повторение части кода, связанного с вариационной задачей. Заново объявляем
+    функции v_1, v_2, u_1, u_2. Вычисляем плотность и давление в узлах сетки,
+    используя файл H1 с пересчитанной энтальпией в узлах.
+    Записываем нннкционал F. Решаем задачу. Строим графики.
+    P.S. Граничнее условия не меняются, сетка не меняется.
+    """
+    v_1, v_2 = TestFunctions(V)
+
+    u = Function(V)
+
+    u_1, u_2 = split(u)
+
+    Name1='data/EOS.txt'
+    Name2='data/H1.txt'
+
+    result=numer_src_function(Name1, Name2,n)
+
+    f = Function(SRC)
+
+    f00 = Function(SRC)
+
+    f.vector()[:] = result[0]
+    f00.vector()[:] = result[1]
+
+    F=-f1*u_1.dx(0)*v_1.dx(0)*dx-f1*f0*f0*u_1.dx(1)*v_1.dx(1)*dx \
+    -f2*u_2.dx(0)*v_2.dx(0)*dx-f2*f0*f0*u_2.dx(1)*v_2.dx(1)*dx \
+    +f1*v_1*u_1.dx(0)*u_2.dx(0)*dx+f1*f0*f0*v_1*u_1.dx(1)*u_2.dx(1)*dx \
+    +f2*v_2*u_2.dx(0)*u_2.dx(0)*dx+f2*f0*f0*v_2*u_2.dx(1)*u_2.dx(1)*dx \
+    -f1*4*(np.pi)*(0*f+3*f00)*exp(2*u_2-2*u_1)*v_1*dx \
+    -f2*exp(2*u_2-2*u_1)*4*4*(np.pi)*f00*v_2*dx
+
+    solve(F == 0, u, bcs)
+
+    theta0=0
+
+    data_x=[]
+
+    data_y1=[]
+
+    data_y2=[]
+
+    for i in range (1000):
+    	x1=0.0001+0.001*i*(R1)
+    	p=Point(x1,theta0)
+    	F1=u(p.x(), p.y())[0]
+    	F2=u(p.x(), p.y())[1]
+    	data_x.append(x1)
+    	data_y1.append(F1)
+    	data_y2.append(F2)
+
+
+    fig=plt.figure(figsize=(8,6), facecolor='pink', frameon=True)
+    plt.plot(data_x,data_y1)
+    plt.savefig('results/Profile1-2.png')
+    plt.show()
+
+    fig=plt.figure(figsize=(8,6), facecolor='pink', frameon=True)
+    plt.plot(data_x,data_y2)
+    plt.savefig('results/Profile2-2.png')
     plt.show()
